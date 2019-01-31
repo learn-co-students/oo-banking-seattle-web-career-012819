6b546df8da61acdeb057501ef4fe84cc064202c6
ScottDenton <scottyd_14@hotmail.com>
2019-01-31 10:38:16 -0800

    Done

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,39 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name, balance = 1000)
+    @name =name
+    @balance = balance
+    @status = "open"
+  end
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    !!(self.status == "open" && self.balance > 0)
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
 end
+
+#
+# can't change its name
+# #deposit
+#  can deposit money into its account (FAILED - 5)
+# #display_balance
+#  can display its balance (FAILED - 6)
+# #valid?
+#  is valid with an open status and a balance greater than 0 (FAILED - 7)
+# #close_account
+#  can close its account (FAILED - 8)
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
 class Transfer
   # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status, :complete
+
+  def initialize(sender, receiver, amount, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    (self.sender.valid? && self.sender.balance > self.amount && self.receiver.valid? && self.status == "pending")
+  end
+
+  def execute_transaction
+    if self.valid?
+      self.sender.balance -= self.amount
+      self.receiver.balance += self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    else
+      "That transfer has already been reversed"
+    end
+  end
+
+
 end
+#
+# Transfer
+#   #valid?
+#     can check that both accounts are valid (FAILED - 6)
+#     calls on the sender and receiver's #valid? methods (FAILED - 7)
+#   #execute_transaction
+#     can execute a successful transaction between two accounts (FAILED - 8)
+#     each transfer can only happen once (FAILED - 9)
+#     rejects a transfer if the sender doesn't have a valid account (FAILED - 10)
+#   #reverse_transfer
+#     can reverse a transfer between two accounts (FAILED - 11)
+#     it can only reverse executed transfers (FAILED - 12)

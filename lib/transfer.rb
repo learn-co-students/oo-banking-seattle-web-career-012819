95508fe3752b6510a7ae43688863ff81040384a3
github username <github email address>
2019-01-31 11:30:53 -0800

    Completed the banking lab with modular move_money function

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,44 @@
+require "pry"
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  @@all = []
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? && @sender.balance >= @amount
+  end
+
+  def execute_transaction
+    if self.valid? && @status != "complete"
+      move_money(@sender, @receiver, @amount, "complete")
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      move_money(@receiver, @sender, @amount, "reversed")
+    end
+  end
+
+  def move_money(from_a, to_b, dollar_amount, set_status)
+    from_a.deposit(-dollar_amount)
+    to_b.deposit(dollar_amount)
+    @status = set_status
+  end
 end

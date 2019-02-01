13e2dfe3422e51ddef80bea7b82e5b03af51e92d
github username <github email address>
2019-01-31 17:56:09 -0800

    need to fill out transaction & reverse

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+
+  end
+
+  def valid?
+    if (@sender.valid?) && (@receiver.valid?)
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if valid? == true
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "Transaction rejected. Please check your account balance."
+    end
+  end
 end
